9ccb3a5643d261336a1849e4a1458d170d25a26d
Ade Suhada <ade.suhada@mail.ugm.ac.id>
2018-11-05 20:38:41 +0700

    commit ke-2

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ use \LINE\LINEBot\SignatureValidator as SignatureValidator;
 $pass_signature = true;
  
 // set LINE channel_access_token and channel_secret
-$channel_access_token = "";
-$channel_secret = "";
+$channel_access_token = "Gw1bZ6cwKxln54UvtUmN1ymqp6/sVk8aMLZPGBbIku3Veh13BmOrDw4x8FrGz6Qz9oHV8jd/mwZtsNpv2vY4pNU0HJyCxdO3HgVEYMMqWXaGH5goOcUM7jv2ldz7L61OsWu4vV/gZxI/qg+ZeUkwfgdB04t89/1O/w1cDnyilFU=";
+$channel_secret = "e2ae44d00a1fb7bdd23b2d66f58a3530";
  
 // inisiasi objek bot
 $httpClient = new CurlHTTPClient($channel_access_token);
@@ -30,8 +30,9 @@ $app->get('/', function($req, $res)
   echo "Welcome at Slim Framework";
 });
  
-// buat route untuk webhook
+// buat route untuk webhook // 
 $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature)
+
 {
     // get request body and line signature header
     $body        = file_get_contents('php://input');
@@ -40,7 +41,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
     // log body and signature
     file_put_contents('php://stderr', 'Body: '.$body);
  
-    if($pass_signature === false)
+    if($pass_signature === true)
     {
         // is LINE_SIGNATURE exists in request header?
         if(empty($signature)){
@@ -54,7 +55,35 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
     }
  
     // kode aplikasi nanti disini
+$data = json_decode($body, true);
+if(is_array($data['event'])){
+    foreach ($data['event'] as $event){
+        if ($evet['type'] == 'message'){
+            if ($event['message']['type'] == 'text'){
+                // send same message as reply to user
+                $result = $bot->replyText($event['replyToken'],$event['message']['text']);
+
+                // or we can use replyMessage() instead to send reply message
+                // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                // $result = $bot->withMESSAGE($event['replyToken'], $textMessageBuilder);
+
+                return $response->withJson($result->getJSONDecodedBody(),$result->getHTTPSatus());
+                
+            }
+        }
+    }
+}
+ 
  
 });
- 
+$app->get('/pushmessage', function($req, $res) use ($bot)
+{
+    // send push message to user
+    $userId = 'jalerse';
+    $textMessageBuilder = new TextMessageBuilder('Halo, ini pesan push');
+    $result = $bot->pushMessage($userId, $textMessageBuilder);
+   
+    return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+});
+
 $app->run();
