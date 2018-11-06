0c0e6169ed14f76a265b5ea87d14c8e66e16058b
Ade Suhada <ade.suhada@mail.ugm.ac.id>
2018-11-06 16:29:59 +0700

    commit ke-22 Implementasi flex message

--- index.php
+++ index.php
@@ -53,38 +53,38 @@ $app->post('/webhook', function (Request $request, Response $response) use ($bot
                         $result     = $bot->replyMessage($event['replyToken'], $multiMessageBuilder);
                         return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
                     }  
-                //message from user
                  }
-
+                }
+                //message from user 
                  } 
-                // else {
-                //     if ($event['message']['type'] == 'text') {
-                //         if (strtolower($event['message']['text']) == 'user id') {
+                else {
+                    if ($event['message']['type'] == 'text') {
+                        if (strtolower($event['message']['text']) == 'user id') {
  
-                //             $result = $bot->replyText($event['replyToken'], $event['source']['userId']);
+                            $result = $bot->replyText($event['replyToken'], $event['source']['userId']);
  
-                //         } elseif (strtolower($event['message']['text']) == 'flex message') {
+                        } elseif (strtolower($event['message']['text']) == 'flex message') {
  
-                //             $flexTemplate = file_get_contents("flex_message.json"); // template flex message
-                //             $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
-                //                 'replyToken' => $event['replyToken'],
-                //                 'messages'   => [
-                //                     [
-                //                         'type'     => 'flex',
-                //                         'altText'  => 'Test Flex Message',
-                //                         'contents' => json_decode($flexTemplate)
-                //                     ]
-                //                 ],
-                //             ]);
+                            $flexTemplate = file_get_contents("flex_message.json"); // template flex message
+                            $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
+                                'replyToken' => $event['replyToken'],
+                                'messages'   => [
+                                    [
+                                        'type'     => 'flex',
+                                        'altText'  => 'Test Flex Message',
+                                        'contents' => json_decode($flexTemplate)
+                                    ]
+                                ],
+                            ]);
  
-                //         } else {
-                //             // send same message as reply to user
-                //             $result = $bot->replyText($event['replyToken'], $event['message']['text']);
-                //         }
+                        } else {
+                            // send same message as reply to user
+                            $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+                        }
  
-                //         return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-                //     }
-                // }
+                        return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+                    }
+                }
             }
         }
     }
