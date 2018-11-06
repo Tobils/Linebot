9fa3eb293e4d1854284ca377aae6d0b89a683c0d
Ade Suhada <ade.suhada@mail.ugm.ac.id>
2018-11-06 13:05:37 +0700

    commit ke-21 Implementasi Content API

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                         $basePath    = $request->getUri()->getBaseUrl();
                         $contentURL  = $basePath."/content/".$event['message']['id'];
                         $contentType = ucfirst($event['message']['type']);
-                        $result = $bot->replyText($event['replyToken'], $contentType. 'yang Anda kirim bisa diakses dari link:\n ' . $contentURL);
+                        $result = $bot->replyText($event['replyToken'], $contentType. "yang Anda kirim bisa diakses dari link:\n " . $contentURL);
                     
                         return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
                     }
